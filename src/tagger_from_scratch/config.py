e85a6e9c21d536026c8100135f342bd449b0acb0
Soldaini, Luca <lssoldai@amazon.com>
2021-10-26 23:20:54 -0700

    config

--- src/tagger_from_scratch/config.py
+++ src/tagger_from_scratch/config.py
@@ -3,7 +3,6 @@ import os
 import dataclasses
 import ast
 
-import ipdb
 
 HOME = os.path.expanduser('~')
 
@@ -36,6 +35,11 @@ def create_config() -> Config:
     ap = argparse.ArgumentParser()
 
     for field in dataclasses.fields(Config):
+        # for each field in the dataclass, make a argument
+        # for the cli. make the argument required if the
+        # config doesn't have a default value. use type
+        # annotations to set the required type (more on that
+        # below).
         if hasattr(Config, field.name):
             field_default = getattr(Config, field.name)
             required = False
@@ -43,17 +47,22 @@ def create_config() -> Config:
             field_default = None
             required = True
 
-        def type_fn(local_field_name, local_field_type, local_field_default):
+        def type_fn(local_field_type):
+            """Cast to type using a combination of ast literal eval
+            and type class. Ignore literal eval if the expected type
+            is string."""
             def _type_fn(x):
                 if not issubclass(local_field_type, str):
                     x = ast.literal_eval(x)
                 return local_field_type(x)
             return _type_fn
 
+        # add the actual argument!
         ap.add_argument(f'--{field.name}',
                         default=field_default,
                         required=required,
-                        type=type_fn(field.name, field.type, field_default))
+                        type=type_fn(field.type))
 
+    # parse options, chuck them in the config.
     opts = ap.parse_args()
     return Config(**opts.__dict__)
